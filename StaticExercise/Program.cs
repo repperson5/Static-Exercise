9ec60bb7d53f465c0d7f946a05874af8296b2380
Rhea Epperson <re.epperson2@gmail.com>
2023-02-11 20:20:33 -0500

    exercise complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is the temp in Fahrenheit you want to convert?");
+            var fahrTemp = Convert.ToDouble(Console.ReadLine());
+            var convertToCel = TempConverter.FahrenheitToCelsius(fahrTemp);
+
+            Console.WriteLine($"That equals {convertToCel} degrees Celsius");
+            Console.WriteLine();
+
+            Console.WriteLine("What is temperature in Celsius you want to convert?");
+            var celTemp = Convert.ToDouble(Console.ReadLine());
+            var convertToFahr = TempConverter.CelsiusToFahrenheit(celTemp);
+
+            Console.WriteLine($"That equals {convertToFahr} degrees Fahrenheit");
+            
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double fTemp)
+		{
+			var celTemp = (fTemp - 32) * (5.0 / 9.0);
+			return celTemp;
+
+		}
+
+        public static double CelsiusToFahrenheit(double cTemp)
+		{
+			var fahrTemp = (cTemp * (9.0 / 5.0) + 32);
+			return fahrTemp;
+		}
+
+    }
+}
+
